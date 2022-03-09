7b8d4ff0cec1568ead611fdb4bc241aee4608f85
tjs <tejas.g@simformsolutions.com>
2022-03-09 17:45:10 +0530

    feat:TE5:T86 crud application

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_03_120452) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_09_120452) do
   create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "brand_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
