5b92e3b22bddc4050cd76b02c1b99de36cc17431
tjs <tejas.g@simformsolutions.com>
2022-04-14 13:56:40 +0530

    Advance active records query interface change

--- app/controllers/product1s_controller.rb
+++ app/controllers/product1s_controller.rb
@@ -1,51 +1,51 @@
 class Product1sController < ApplicationController
-  before_action :find_product, only: [:show, :edit, :update, :destroy]
+before_action :find_product, only: [:show, :edit, :update, :destroy]
 
-    def index
-      @product1s = Product1.all
-    end
+  def index
+    @product1s = Product1.all
+  end
   
-    def new
-      @product1 = Product1.new
-    end
+  def new
+    @product1 = Product1.new
+  end
   
-    def create
-      @product1 = Product1.create(product1_parmas)
-      if @product1.valid?
-        redirect_to product1s_path
-      else
-        flash[:errors] = @product1.errors.full_messages
-        redirect_to product1s_path
-      end
+  def create
+    @product1 = Product1.create(product1_parmas)
+    if @product1.valid?
+      redirect_to product1s_path
+    else
+      flash[:errors] = @product1.errors.full_messages
+      redirect_to product1s_path
     end
+  end
   
-    def show
-    end
+  def show
+  end
   
-    def edit
-    end
+  def edit
+  end
 
-    def show_product
-      @product1=Product1.all
-    end
+  def show_product
+    @product1=Product1.all
+  end
   
-    def update
-      @product1.update(product1_parmas)
-      redirect_to product1s_path(@product1)
-    end
+  def update
+    @product1.update(product1_parmas)
+    redirect_to product1s_path(@product1)
+  end
   
-    def destroy
-      @product1 = Product1.unscoped.find(params[:id])
-      if @product1.destroy
-        redirect_to product1s_path
-      end
-    
-    def product1_parmas
-      params.require(:product1).permit(:title,:description, :price, :capacity, :is_active, :status)
+  def destroy
+    @product1 = Product1.unscoped.find(params[:id])
+    if @product1.destroy
+      redirect_to product1s_path
     end
+  end
 
-    def find_product
-      @product1 = Product1.unscoped.find(params[:id])
-    end
+  def product1_parmas
+    params.require(:product1).permit(:title,:description, :price, :capacity, :is_active, :status)
+  end
+
+  def find_product
+    @product1 = Product1.unscoped.find(params[:id])
   end
 end 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  enum status: {booked:0,cancelled:1  }
-  belongs_to :product1
+  enum status: {booked:0 , cancelled:1}
+  belongs_to :product1  
   belongs_to :customer  
 end
